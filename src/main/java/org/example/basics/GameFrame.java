ba10fd6a5df4420d6ac097d6aa55dcfe465c6ad1
JirsákFrantišek <jirsak.f@ssmg.cz>
2026-03-31 11:27:03 +0200

    fixed gameframe and enemy spawning method to work when instructed in gameframe

--- src/main/java/org/example/basics/GameFrame.java
+++ src/main/java/org/example/basics/GameFrame.java
@@ -4,6 +4,7 @@ import org.example.characters.Enemy;
 import org.example.characters.Player;
 import org.example.gamefield.Background;
 import org.example.inputs.KeyInput;
+import org.example.inputs.MouseInput;
 import org.example.menu.Button;
 
 import javax.swing.*;
@@ -15,9 +16,10 @@ public class GameFrame extends JPanel {
 
 
     private static final int timeDelay = 16;
+    private final MouseInput mouseInput = new MouseInput();
     private final KeyInput keyInput = new KeyInput();
     Player player = new Player(60,800, keyInput);
-    Enemy enemy;
+    Enemy enemy = new Enemy(0,0);
     ArrayList<Enemy> enemies = new ArrayList<>();
     Button button = new Button();
     Background background = new Background(this);
@@ -26,6 +28,7 @@ public class GameFrame extends JPanel {
 
         setFocusable(true);
         addKeyListener(keyInput);
+        addMouseListener(mouseInput);
         requestFocusInWindow();
 
         new Timer(timeDelay, e -> {
@@ -33,7 +36,9 @@ public class GameFrame extends JPanel {
             player.movePlayer();
             player.onBorders();
 
-
+            for (Enemy enemy : enemies){
+                enemy.moveEnemy();
+            }
 
             if (keyInput.isKeyPressed(KeyEvent.VK_P)){
                 switchMenu = !switchMenu;
@@ -68,13 +73,17 @@ public class GameFrame extends JPanel {
     private void paintGame(Graphics g){
         background.drawBackground(g);
         player.drawPlayer(g);
-        enemy.spawnEnemies();
+        for (Enemy e : enemies){
+            e.spawnEnemies();
+            e.drawEnemy(g);
+        }
+
+
 
 
 
 
     }
-
 
     public boolean isSwitchMenu() {
         return switchMenu;
--- src/main/java/org/example/characters/Enemy.java
+++ src/main/java/org/example/characters/Enemy.java
@@ -5,20 +5,20 @@ import org.example.basics.GameFrame;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.Objects;
 import java.util.Random;
 
 public class Enemy extends Entita {
-
     private int moveSpeed = 3;
     GameFrame gameFrame;
+    ArrayList<Enemy> enemies = new ArrayList<>();
     Player player;
-    ArrayList<Entita> enemies = new ArrayList<>();
 
-    private final Image ENEMY_1 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/background/guntime_mission_background.png"))).getImage();
-    private final Image ENEMY_2 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/background/guntime_mission_background.png"))).getImage();
-    private final Image ENEMY_3 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/background/guntime_mission_background.png"))).getImage();
-    private final Image ENEMY_4 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/background/guntime_mission_background.png"))).getImage();
+    private final Image ENEMY_1 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/characters/mc_soldier_1.png"))).getImage();
+    private final Image ENEMY_2 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/characters/mc_soldier_2.png"))).getImage();
+    private final Image ENEMY_3 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/characters/mc_soldier_3.png"))).getImage();
+    private final Image ENEMY_4 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/characters/mc_soldier_4.png"))).getImage();
 
     private Image currentImage = ENEMY_1;
 
@@ -26,38 +26,39 @@ public class Enemy extends Entita {
         super(x, y);
     }
 
+    public void drawEnemy(Graphics g){
+        g.drawImage(currentImage, x, y, null);
+    }
+
     public void moveEnemy(){
         for (Entita enemy : enemies){
             if (player.x > enemy.x){
-                enemy.x = enemy.x - moveSpeed;
+                enemy.x += moveSpeed;
             }
-
             if (player.x < enemy.x){
-                enemy.x = enemy.x + moveSpeed;
+                enemy.x -= moveSpeed;
             }
-
             if (player.y > enemy.y){
-                enemy.y = enemy.y + moveSpeed;
+                enemy.y += moveSpeed;
             }
-
             if (player.y < enemy.y){
-                enemy.y = enemy.y - moveSpeed;
+                enemy.y -= moveSpeed;
             }
 
         }
 
     }
 
-    public void spawnEnemies(){
+    public void spawnEnemies() {
         Random random = new Random();
-
         for (int i = 0; i < 49; i++){
-            int x = random.nextInt(1500, 2000);
-            int y = random.nextInt(-300, -150);
-
+            int x = random.nextInt(1000, 1200);
+            int y = random.nextInt(100, 200);
             enemies.add(new Enemy(x, y));
         }
     }
+
+
 
 
 
