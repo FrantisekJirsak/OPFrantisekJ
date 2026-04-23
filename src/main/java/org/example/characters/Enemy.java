7af66cd67477af73247e582048309f08a1132b0a
FrantisekJirsak <jirsak.f@ssmg.cz>
2026-04-23 23:05:02 +0200

    fuctioning game, only need to fix the movement of the enemies and add a win scenario. other than that, the coding project will be complete. after that comes the optimization :3

---
+++ src/main/java/org/example/characters/Enemy.java
@@ -0,0 +1,52 @@
+package org.example.characters;
+
+import org.example.basics.GameFrame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+public class Enemy extends Entita {
+    private int moveSpeed = 1;
+    Player player;
+
+    public Enemy(int x, int y, Player player) {
+        super(x, y, player.dx, player.dy);
+        this.player = player;
+    }
+
+    private final Image ENEMY_1 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/characters/mc_soldier_4.png"))).getImage();
+    private final Image ENEMY_2 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/characters/mc_soldier_3.png"))).getImage();
+    private final Image ENEMY_3 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/characters/mc_soldier_2.png"))).getImage();
+    private final Image ENEMY_4 = new ImageIcon(Objects.requireNonNull(GameFrame.class.getResource("/characters/mc_soldier_1.png"))).getImage();
+
+    private Image currentImage = ENEMY_1;
+
+    public void drawEnemies(Graphics g) {
+        g.drawImage(currentImage, x, y, null);
+    }
+
+    public void moveEnemy() {
+        if (player.x > x) {
+            x += moveSpeed;
+            currentImage = ENEMY_1;
+        }
+        if (player.x < x) {
+            x -= moveSpeed;
+            currentImage = ENEMY_2;
+        }
+        if (player.y > y) {
+            y += moveSpeed;
+            currentImage = ENEMY_3;
+        }
+        if (player.y < y) {
+            y -= moveSpeed;
+            currentImage = ENEMY_4;
+        }
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, 32, 64);
+    }
+
+}
